5ecac90cf065dcdd66ccbb6c0b6a8e765a1706b1
Mike Millar <39285355+MikeMillar@users.noreply.github.com>
2019-07-14 20:25:01 -0600

    int and string arrays

--- Arrays/Arrays.cpp
+++ Arrays/Arrays.cpp
@@ -1,20 +1,42 @@
-// Arrays.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Array of integers" << endl;
+	cout << "_________________" << endl;
+	int values[3];
+	values[0] = 88;
+	values[1] = 123;
+	values[2] = 7;
+	cout << values[0] << endl;
+	cout << values[1] << endl;
+	cout << values[2] << endl;
+
+	cout << endl << "Array of doubles" << endl;
+	cout << "________________" << endl;
+	double numbers[4] = {4.5, 2.3, 7.2, 8.1};
+	for (int i = 0; i < 4; i++) {
+		cout << "Element at index " << i << ": " << numbers[i] << endl;
+	}
+
+	cout << endl << "Initializing with zero values" << endl;
+	cout << "________________" << endl;
+	int numberArray[8] = {};
+
+	for (int i = 0; i < 8; i++) {
+		cout << "Element at index " << i << ": " << numberArray[i] << endl;
+	}
+
+	//Array of strings
+	string texts[] = {"apple", "banana", "orange"};
+	for (int i = 0; i < 3; i++) {
+		cout << "Element at index " << i << ": " << texts[i] << endl;
+	}
+
+
+
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
